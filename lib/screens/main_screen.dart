e48f3c986bff917eb4b11f4ca90a181fec083d2c
Enrico Cecchetti <enrico.cecchetti@kcitylabs.fr>
2025-05-12 19:20:56 +0200

    add provider, add recipe searchbar

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:cooka/widgets/recipe_list.dart';
-import 'package:cooka/data/dummy.dart';
+import 'package:cooka/providers/recipe_provider.dart';
 
-class MainViewScreen extends StatelessWidget {
+class MainViewScreen extends ConsumerStatefulWidget {
   const MainViewScreen({super.key});
 
   @override
+  ConsumerState<MainViewScreen> createState() => _MainViewScreenState();
+}
+
+class _MainViewScreenState extends ConsumerState<MainViewScreen> {
+  late TextEditingController searchController;
+
+  @override
+  void initState() {
+    super.initState();
+    searchController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final recipeList = ref.watch(filteredRecipesProvider);
 
     return Scaffold(
       appBar: AppBar(
@@ -17,10 +38,10 @@ class MainViewScreen extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               TextField(
+                controller: searchController,
                 decoration: InputDecoration(
                   hintText: 'Search...',
                   prefixIcon: Icon(Icons.search),
@@ -34,12 +55,19 @@ class MainViewScreen extends StatelessWidget {
                   filled: true,
                   fillColor: Colors.grey[200],
                 ),
+                onChanged: (value) {
+                  ref.read(searchQueryProvider.notifier).state = value;
+                },
               ),
               const SizedBox(height: 20),
-              RecipListTile(title: 'Recent Receips', recipes: recipeList, showTitle: true,),
+              RecipListTile(
+                title: 'Recent Receips',
+                recipes: recipeList,
+                showTitle: true,
+              ),
               RecipListTile(
                 title: 'Friends Receips',
-                recipes: friendReceipts,
+                recipes: recipeList,
                 showTitle: true,
               ),
             ],
